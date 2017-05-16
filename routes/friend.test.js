a885d84b5377111c2d961219ebaa552089780fa4
Hu Qiang <nickhuq@gmail.com>
2017-05-16 22:34:27 +0800

    Added positive test cases.  Changes to be committed: 	modified:   routes/friend.test.js

--- routes/friend.test.js
+++ routes/friend.test.js
@@ -60,6 +60,7 @@ describe('FriendsAPI', () => {
         res.body.success.should.equal(true);
         res.body.friends.should.be.a('array');
         res.body.friends.length.should.eql(1);
+        res.body.friends.should.include('john@example.com');
         done();
       });
     });
@@ -110,6 +111,58 @@ describe('FriendsAPI', () => {
         res.body.success.should.equal(true);
         res.body.friends.should.be.a('array');
         res.body.friends.length.should.eql(1);
+        res.body.friends.should.include('common@example.com');
+        done();
+      });
+    });
+
+    it('it should add to subscribe list', (done) => {
+      chai.request(server)
+      .post('/api/subscribeUpdates')
+      .send({
+        requestor: "lisa@example.com",
+        target:"john@example.com"
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.success.should.equal(true);
+        done();
+      });
+    });
+
+    it('it should add to a block list', (done) => {
+      chai.request(server)
+      .post('/api/blockUpdates')
+      .send({
+        requestor: "andy@example.com",
+        target: "john@example.com"
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.success.should.equal(true);
+        done();
+      });
+    });
+
+    it('it should get correct recipient list', (done) => {
+      chai.request(server)
+      .post('/api/getRecipients')
+      .send({
+        sender: "john@example.com",
+        text: "Hello World! kate@example.com"
+      })
+      .end((err, res) => {
+        console.log(res.body);
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.success.should.equal(true);
+        res.body.recipients.should.be.a('array');
+        res.body.recipients.length.should.eql(3);
+        res.body.recipients.should.include('lisa@example.com');
+        res.body.recipients.should.include('kate@example.com');
+        res.body.recipients.should.include('common@example.com');
         done();
       });
     });
